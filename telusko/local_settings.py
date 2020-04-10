15da1716d650d101bbeb57f5011d483487cbc409
Ammolla Karunakar <karunakarammolla@gmail.com>
2020-04-10 19:00:46 +0530

    added dj-database-url package

--- telusko/local_settings.py
+++ telusko/local_settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 
+import dj_database_url
 
 import os
 
@@ -93,7 +94,9 @@ DATABASES = {
     }
 }
 
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env )
+DATABASES = {'default': dj_database_url.config()}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
--- telusko/settings.py
+++ telusko/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -93,7 +93,9 @@ DATABASES = {
 
     }
 }
-import dj_database_url
+
+
+
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env )
 
