e4c5e2bb2fd6078195d4fc5b85380ced717fd088
Ammolla Karunakar <35535623+karunkarammolla@users.noreply.github.com>
2020-04-11 04:27:30 +0530

    Update settings.py

--- telusko/settings.py
+++ telusko/settings.py
@@ -98,13 +98,14 @@ WSGI_APPLICATION = 'telusko.wsgi.application'
 #
 #     }
 # }
-# DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
